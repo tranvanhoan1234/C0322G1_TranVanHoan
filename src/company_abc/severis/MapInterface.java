1f9b8190031c002425c1d9a2983632dd90419431
tranvanhoan1234 <89346268+tranvanhoan1234@users.noreply.github.com>
2022-05-23 21:49:00 +0700

    up

--- src/company_abc/severis/MapInterface.java
+++ src/company_abc/severis/MapInterface.java
@@ -14,6 +14,7 @@ public class MapInterface {
 
 
     static {
+
         NhanVienCongNhat companyABCList1 = new NhanVienCongNhat("hoàn", "2/3/1999", "đn", 5);
         NhanVienSanXuat companyABCList2 = new NhanVienSanXuat("thuan", "2/3/1999", "đn", 10);
         StaffOfficials companyABCList3 = new StaffOfficials("phuc", "2/3/1999", "đn", 10, 5);
@@ -26,12 +27,23 @@ public class MapInterface {
     }
 
     //    Làm sao để kiểm tra mã nhân viên đã tồn tại hay chưa?
-// dung == vì kiểu key là int
+// dung containkey kiểm tra tồn tại chưa nếu tồn tại trả về true k false
     public static void displayMap() {
+
         Set<Integer> a = companyABCMap.keySet();
         for (Integer b : a) {
             System.out.println("Key: " + b);
             System.out.println("Value: " + companyABCMap.get(b));
+            System.out.println("mã này đã tồn tại =>" + companyABCMap.containsKey(4));
         }
+//    }public boolean check(int id){
+//        if(companyABCMap.containsKey(id)){
+//            System.out.println("id đã tồn tại");
+//            return true;
+//        }
+//        else {
+//            System.out.println("id k tồn tại");
+//            return false;
+//        }
     }
 }
